d6d9e3f9bca5164828d11cc97bea38ccfa02b2e2
yodalainen <andreas@andreasolofsson.nu>
2026-01-20 10:22:47 +0100

    Add debug logging for request URL in middleware and include test noscript tag in index.astro

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
   const url = new URL(context.request.url);
+  console.log("url.pathname", url.pathname);
   
   // 0. Exclude the root path from Basic Auth
   if (url.pathname === "/" || url.pathname === "") {
